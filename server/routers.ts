2fecf85b0655c509487ce15accdfaede4606d106
fullpropt <beachhtadhgdh00@hotmail.com>
2025-12-09 13:25:09 -0300

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -5,6 +5,22 @@ import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, router } from "./_core/trpc";
 
 export const appRouter = router({
+  // ... outras rotas ...
+  
+  autoSend: {
+    getStatus: publicProcedure.query(async () => {
+      return await getAutoSendStatus();
+    }),
+    
+    toggle: publicProcedure
+      .input(z.boolean())
+      .mutation(async ({ input }) => {
+        return await toggleAutoSend(input);
+      }),
+  },
+});
+
+export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
   system: systemRouter,
   auth: router({
